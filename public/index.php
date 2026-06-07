9ad25fe2e99aac55908cb928d46ff6ddc0f79607
Pudding Hamster <hamster@example.com>
2026-06-07 17:20:37 +0900

    Feat: public > index.php 파일 수정, src > pages 폴더 및 파일 생성

--- public/index.php
+++ public/index.php
@@ -1,3 +1,24 @@
 <?php
-echo "<h1>내 PHP 포트폴리오 시작!</h1>";
-?>
+include '../src/bootstrap.php';
+
+$raw_uri = urldecode($_SERVER['REQUEST_URI']);
+$path = mb_strtolower($raw_uri, 'UTF-8');
+$path = substr($path, strlen(DOC_ROOT));
+$parts = explode('/', $path);
+
+if ($parts[0] != 'admin') {
+    $page = $parts[0] ?: 'index';
+    $id = $parts[1] ?? null;
+} else {
+    $page = 'admin/' . ($parts[1] ?? '');
+    $id = $parts[2] ?? null;
+}
+$id = filter_var($id, FILTER_VALIDATE_INT);
+
+$php_page = APP_ROOT . '/src/pages' . $page . '.php';
+
+if (!file_exists($php_page)) {
+    $php_page = APP_ROOT . '/src/pages/page-not-found.php';
+}
+
+include $php_page;
