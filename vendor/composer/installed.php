5e1552e958046ef1f2a6679ac11701b6bb2c81a0
pyuding-ham <hamster@example.com>
2026-06-10 22:34:47 +0900

    chore: 비밀번호 찾기 시 이메일 전송을 위한 PHPMailer 설치

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '33d764e8eb46c08d2f169533c0e23b912bde9713',
+        'reference' => '394c9305f049c2584f72cd8fd76cba1b7af2a6ac',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '33d764e8eb46c08d2f169533c0e23b912bde9713',
+            'reference' => '394c9305f049c2584f72cd8fd76cba1b7af2a6ac',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
