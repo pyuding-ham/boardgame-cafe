52faede9e050b4a200701710fe2c680b85174fdd
pyuding-ham <hamster@example.com>
2026-06-23 00:36:58 +0900

    refactor(pages/auth-check-username): 불필요한 필수값/유효성 검사를 제거하고 중복 여부 반환 기능만 남기도록 수정

--- src/pages/auth-check-username.php
+++ src/pages/auth-check-username.php
@@ -1,35 +1,9 @@
 <?php
 declare(strict_types = 1);
 
-use BoardgameCafe\Validate\Validate;
-
 header('Content-Type: application/json');
 
 $username = trim($_GET['username'] ?? '');
-
-// 필수 입력 값 검사
-if (empty($username)) {
-    http_response_code(400);
-
-    echo json_encode([
-        'status' => 'empty',
-        'message' => '아이디를 입력해 주세요.'
-    ], JSON_UNESCAPED_UNICODE);
-    exit;
-}
-
-// 아이디 유효성 검사
-if (!Validate::isUsername($username)) {
-    http_response_code(400);
-
-    echo json_encode([
-        'status' => 'invalid',
-        'message' => '아이디는 4~20자의 영문, 숫자, 언더바(_)만 가능합니다.'
-    ], JSON_UNESCAPED_UNICODE);
-
-    exit;
-}
-
 $sql = "SELECT id FROM user WHERE username = :username;";
 $stmt = $cms->getDb()->runSql($sql, ['username' => $username]);
 
