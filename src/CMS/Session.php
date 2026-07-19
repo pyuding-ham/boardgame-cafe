29bb43d0651a91f0321daaac4a006a539537f2b1
pyuding-ham <hamster@example.com>
2026-07-20 01:29:04 +0900

    fix(Session): 비로그인 상태에서 비밀번호 변경 시 세션 파괴(session_destroy) 에러 수정

--- src/CMS/Session.php
+++ src/CMS/Session.php
@@ -47,17 +47,19 @@ class Session
      */
     public function delete()
     {
-        $_SESSION = [];
-        $param = session_get_cookie_params();
-        setcookie(
-            session_name(),
-            '',
-            time() - 2400,
-            $param['path'],
-            $param['domain'],
-            $param['secure'],
-            $param['httponly']
-        );
-        session_destroy();
+        if (session_status() === PHP_SESSION_ACTIVE) {
+            $_SESSION = [];
+            $param = session_get_cookie_params();
+            setcookie(
+                session_name(),
+                '',
+                time() - 2400,
+                $param['path'],
+                $param['domain'],
+                $param['secure'],
+                $param['httponly']
+            );
+            session_destroy();
+        }
     }
 }
