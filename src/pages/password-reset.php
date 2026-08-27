4f6e06dbc10291378a24f8d38b554c689185ceaa
pyuding-ham <hamster@example.com>
2026-08-27 22:25:44 +0900

    feat: 비밀번호 변경 화면 디자인 변경 및 안내 문구 추가

--- src/pages/password-reset.php
+++ src/pages/password-reset.php
@@ -41,7 +41,7 @@ if (empty($errors['invalid_token']) && $_SERVER['REQUEST_METHOD'] === 'POST') {
 
     // 변경할 비밀번호 유효성 검사
     if (empty($password['new'])) {
-        $errors['password'] = '변경 비밀번호를 입력해 주세요.';
+        $errors['password'] = '변경 비밀번호를 입력해 주세요.<br>변경 비밀번호는 최소 10자 이상이어야 하며 영문과 숫자를 모두 포함해야 합니다.<br>(영문/숫자/특수문자 사용 가능)';
     } elseif (!Validate::isPassword($password['new'])) {
         $errors['password'] = '변경 비밀번호는 최소 10자 이상이어야 하며 영문과 숫자를 모두 포함해야 합니다.<br>(영문/숫자/특수문자 사용 가능)';
     }
--- src/pages/register.php
+++ src/pages/register.php
@@ -37,7 +37,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 
     if (empty($user['password'])) {
-        $errors['password'] = '비밀번호를 입력해 주세요.';
+        $errors['password'] = '비밀번호를 입력해 주세요.<br>비밀번호는 최소 10자 이상이어야 하며 영문과 숫자를 모두 포함해야 합니다.<br>(영문/숫자/특수문자 사용 가능)';
     } elseif (!Validate::isPassword($user['password'])) {
         $errors['password'] = '비밀번호는 최소 10자 이상이어야 하며 영문과 숫자를 모두 포함해야 합니다.<br>(영문/숫자/특수문자 사용 가능)';
     }
